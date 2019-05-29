734feabb46cfc47ebd808b215873eaea9b9eae27
Osama A <41015632+codingXllama@users.noreply.github.com>
2019-05-28 23:50:47 -0600

    finding sum of a number
    an iterative approach of finding a sum of a number

---
+++ Java/src/SumOfNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumOfNumber {
+    public static void main(String[] args) {
+        System.out.println("Enter a number to sum to: ");
+        Scanner userIn = new Scanner(System.in);
+        System.out.println("The sum is: " + ComputeSum(userIn.nextInt()));
+
+    }
+
+    public static int ComputeSum(int numbers) {
+        int totalSum = 0;
+        for (int counter = 0; counter < numbers; counter--) {
+            totalSum += counter;
+        }
+        return totalSum;
+    }
+}
